45930dd4cb7e8ac728e0f8e68cda00df4e255751
joshcodes <josh@blackbarlabs.com>
2016-03-09 12:58:13 -0600

    initial

---
+++ CredentialValidationMethodTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Security.Authorization
+{
+    public enum CredentialValidationMethodTypes
+    {
+        OpenIdConnect,
+        Facebook,
+        Twitter,
+        Google,
+        Implicit,
+        Voucher,
+    }
+}
---
+++ IAuthorization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Security.Authorization
+{
+    public interface IAuthorization
+    {
+        [DataMember]
+        Guid Id { get; set; }
+
+        [DataMember]
+        CredentialsType[] CredentialProviders { get; set; }
+
+    }
+}
