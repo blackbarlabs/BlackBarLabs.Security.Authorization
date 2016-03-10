345cbc829d06e1e7901b5eaf8cd5604f79bbea1c
joshcodes <josh@blackbarlabs.com>
2016-03-10 10:28:32 -0600

    Adding session

---
+++ ISession.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Security.Authorization
+{
+    public interface ISession
+    {
+        [DataMember]
+        Guid Id { get; set; }
+
+        [IgnoreDataMember]
+        Guid AuthorizationId { get; set; }
+
+        [DataMember]
+        AuthHeaderProps SessionHeader { get; set; }
+
+        [DataMember]
+        CredentialsType Credentials { get; set; }
+
+        [DataMember]
+        string RefreshToken { get; set; }
+    }
+}
