2b3b65992001f210a7841070f8c4817f0023a924
joshcodes <josh@blackbarlabs.com>
2016-03-10 14:21:50 -0600

    added authheaderprops

---
+++ AuthHeaderProps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Security.Authorization
+{
+    [DataContract]
+    public class AuthHeaderProps
+    {
+        #region Properties
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Value { get; set; }
+        #endregion
+    }
+}
