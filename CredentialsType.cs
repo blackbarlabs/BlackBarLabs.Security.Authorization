16c383d056214a6b98d44c58306975a34db5515c
joshcodes <josh@blackbarlabs.com>
2016-03-21 01:52:32 -0500

    separating out credential as its own resource

--- CredentialsType.cs
+++ CredentialsType.cs
@@ -3,34 +3,34 @@ using System.Runtime.Serialization;
 
 namespace BlackBarLabs.Security.Authorization
 {
-    [DataContract]
-    public class CredentialsType
-    {
-        #region Properties
+    //[DataContract]
+    //public class CredentialsType
+    //{
+    //    #region Properties
 
-        [DataMember]
-        public CredentialValidationMethodTypes Method { get; set; }
+    //    [DataMember]
+    //    public CredentialValidationMethodTypes Method { get; set; }
 
-        [DataMember]
-        public Uri Provider { get; set; }
+    //    [DataMember]
+    //    public Uri Provider { get; set; }
 
-        [DataMember]
-        public string UserId { get; set; }
+    //    [DataMember]
+    //    public string UserId { get; set; }
 
-        [DataMember]
-        public string Token { get; set; }
+    //    [DataMember]
+    //    public string Token { get; set; }
 
-        [DataMember]
-        public Uri[] ClaimsProviders { get; set; }
+    //    [DataMember]
+    //    public Uri[] ClaimsProviders { get; set; }
 
-        #endregion
+    //    #endregion
 
-        public bool IsPopulated()
-        {
-            return
-                (Provider != default(Uri)) &&
-                (!String.IsNullOrWhiteSpace(UserId)) &&
-                (!String.IsNullOrWhiteSpace(Token));
-        }
-    }
+    //    public bool IsPopulated()
+    //    {
+    //        return
+    //            (Provider != default(Uri)) &&
+    //            (!String.IsNullOrWhiteSpace(UserId)) &&
+    //            (!String.IsNullOrWhiteSpace(Token));
+    //    }
+    //}
 }
--- ICredential.cs
+++ ICredential.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlackBarLabs.Security.Authorization
 {
     public interface ICredential
     {
+        [DataMember]
+        Guid AuthorizationId { get; set; }
+
         [DataMember]
         CredentialValidationMethodTypes Method { get; set; }
 
